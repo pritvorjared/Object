f494633db9dcc37cdcca3ef5b32b7a2adfe745ea
pritvorjared <pritvorjared@gmail.com>
2022-02-13 22:18:49 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: "black",
+    bg: "red",
+  },
+  makeTest: function () {
+    console.log("Test");
+  },
+};
+
+options.makeTest();
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// Перебор объекта через for in
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// деструктуризация
+
+const { border, bg } = options.colors;
+console.log(border);
